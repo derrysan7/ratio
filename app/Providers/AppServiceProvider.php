3979003581a1126b8773a73f9c37236a68d2f008
Derry Santoso <derrysantoso7@gmail.com>
2017-06-09 13:15:44 +0700

    pass array with method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('layouts.sidebar', function ($view){
-            $view->with('archives', \App\Post::archives());
-            $view->with('tags', \App\Tag::has('posts')->pluck('name'));
+            //pass array with method, cleaner solution
+            $archives = \App\Post::archives();
+            $tags = \App\Tag::has('posts')->pluck('name');
+            $view->with(compact('archives','tags'));
+            
+            // $view->with('archives', \App\Post::archives());
+            // $view->with('tags', \App\Tag::has('posts')->pluck('name'));
         });
     }
 

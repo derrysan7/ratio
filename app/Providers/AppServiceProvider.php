cb2da45b3285d83c66dc32b4cdbabce4335dfe97
Derry Santoso <derrysantoso7@gmail.com>
2017-06-09 13:12:13 +0700

    display tag only if it has a post

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         view()->composer('layouts.sidebar', function ($view){
             $view->with('archives', \App\Post::archives());
-            $view->with('tags', \App\Tag::pluck('name'));
+            $view->with('tags', \App\Tag::has('posts')->pluck('name'));
         });
     }
 
